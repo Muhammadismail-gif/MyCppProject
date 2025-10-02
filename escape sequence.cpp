df7af81a729eb64d7f43223405b1bb0af8ef8711
Muhammad Ismail <mi.ismail000@gmail.com>
2025-10-02 12:41:59 +0500

    i add escape sequence

---
+++ escape sequence.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello\a world"<<endl;
+	cout<<"hello\b world"<<endl;
+	cout<<"hello\f world"<<endl;
+	cout<<"hello\n world"<<endl;
+	cout<<"hello\r world"<<endl;
+	cout<<"hello\t world"<<endl;
+	cout<<"hello\'world\'"<<endl;
+	cout<<"hello\\world\\"<<endl;
+	return 0;
+}
