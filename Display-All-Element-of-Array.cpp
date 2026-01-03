2bb4e6c224b233fa95b4ada6cc47f666183f3805
Muhammad Ismail <mi.ismail000@gmail.com>
2026-01-03 09:51:24 +0500

    Add:  Display-All-Element-of-Array

---
+++ Display-All-Element-of-Array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int arr[12] = {15, 18, 22, 24, 25, 28, 32, 40, 0, 0, 0, 0};
+int valid = 8;
+    for (int i; i<valid; i++){
+    	cout<<arr[i] << " ";
+	}
+
+}
