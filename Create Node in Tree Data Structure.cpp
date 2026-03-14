132fb46a83f77b2389bc1fdaa3722eb704c60e7a
Muhammad Ismail <mi.ismail000@gmail.com>
2026-03-14 12:39:55 +0500

    Add: Create Node in Tree Data Structure

---
+++ Create Node in Tree Data Structure.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value)
+{
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
