84ab5ed0253c7eff045d9a77f975bb1f3e4103a4
Muhammad Ismail <mi.ismail000@gmail.com>
2026-03-26 15:56:54 +0500

    Add: some basic files

---
+++ 01_Basics/01_simple C++ Program.cpp
@@ -0,0 +1,13 @@
+#include <iostream>   //A Library It allows us to use input/output
+
+using namespace std;  // This tells C++: “I want to use standard tools easily”
+                     //Without this, you must write std::cout
+
+int main()            // starting point of the program
+ {   
+                    // Every C++ program must start here
+                    //  curly brackets They define the body (area) of the program
+    cout << "Assalam-o-Alaikum";  // Prints message on screen
+    
+    return 0;        // Ends program successfully
+}
