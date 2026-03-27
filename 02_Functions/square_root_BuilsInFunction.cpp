8a2b2ad010bceaa79782e0880d904094419ca953
Muhammad Ismail <mi.ismail000@gmail.com>
2026-03-27 16:32:29 +0500

    Add square root Build in Function

---
+++ 02_Functions/square_root_BuilsInFunction.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+ double number = 16.0; // Change this number to find its square root
+ double squareRoot = sqrt(number);
+ cout << "Square root of " << number << " = " << squareRoot << endl;
+ return 0;
+} //output: Square root of 16 = 4
