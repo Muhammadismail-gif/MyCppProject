2009c6891c48770f4fe8e0bcb90a0f4b40043498
Muhammad Ismail <mi.ismail000@gmail.com>
2026-03-31 09:05:39 +0500

    Add: Arithmetic operations.cpp

---
+++ 01_Basics/06_escape sequence.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello\a world"<<endl; // Alert or Alarm
+	cout<<"hello\b world"<<endl; // Backspace
+	cout<<"hello\f world"<<endl; // Form Feed
+	cout<<"hello\n world"<<endl; // New Line
+	cout<<"hello\r world"<<endl; // Carriage Return
+	cout<<"hello\t world"<<endl; //Tab
+	cout<<"hello\'world\'"<<endl; // Single Quote
+	cout<<"hello\\world\\"<<endl; // Two Backslash
+	return 0;
+}
---
+++ 01_Basics/07_Arithmetic operations.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number1;
+int number2;
+number1 = 10;
+number2 = 5;
+cout<<" number1 + number2 =" <<number1 +
+number2<<endl;
+cout<<" number1 - number2 =" <<number1 -
+number2<<endl;
+cout<<" number1 * number2 = "<<number1 *
+number2<<endl;
+cout<< "number1 / number2 = "<<number1 /
+number2<<endl;
+cout<< "number1 % number2 = "<<number1 % number2;
+return 0;
+}
