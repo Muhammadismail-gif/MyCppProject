31043a763987c633f185f0b380132ef5b000cea7
Muhammad Ismail <mi.ismail000@gmail.com>
2026-04-28 14:53:59 +0500

    Add a Operator Overloading

---
+++ 04_OOP/07_Operator Overloading.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Number {
+private:
+    int value;
+
+public:
+    // Constructor
+    Number(int v) {
+        value = v;
+    }
+
+    // Operator Overloading
+    Number operator + (Number obj) {
+        Number temp(0);
+        temp.value = value + obj.value;
+        return temp;
+    }
+
+    void display() {
+        cout << "Value: " << value << endl;
+    }
+};
+
+int main() {
+    Number n1(5), n2(3);
+
+    Number n3 = n1 + n2; // calling overloaded operator
+
+    n3.display();
+
+    return 0;
+}
