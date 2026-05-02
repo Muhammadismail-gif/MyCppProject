adb494050e5762a69ed02008281cdb24540e13f2
Muhammad Ismail <mi.ismail000@gmail.com>
2026-05-02 11:38:01 +0500

    add a class Distance of Overloading Binary Operator

---
+++ 04_OOP/07_04_Overloading Binary Operator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class Distance { //represents distances in feet and inches
+public:
+ int feet, inch;
+// Default constructor initializes the object’s variables (feet and inch) to 0
+ Distance() : feet(0), inch(0)
+ {}
+ // Parametrized constructor initializes the object’s variables to provided 
+value
+ Distance(int f, int i) : feet(f), inch(i)
+ {}
+// Overloading (+) operator to perform addition of two distance objects 
+i.e. d1 (current object, this) and d2 (as a reference)
+ Distance operator+(Distance& d2) {
+ // Create an object to return
+ Distance d3;
+ d3.feet = feet + d2.feet;
+ d3.inch =inch + d2.inch;
+ return d3; // Return the resulting object
+ } }
